ee50abb695556475bcbf9251415fe4d83ffa8470
Blestdon <blestchinonso@gmail.com>
2025-05-11 05:14:48 -0700

    assignment 3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What did you score in your grade percentage? ");
+        string responce = Console.ReadLine();
+        int percent = int.Parse(responce);
+
+        string grade = " ";
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed");
+        }
+        else
+        {
+            Console.WriteLine("Do better next time!");
+        }
+        
     }
+    
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // where the user picks the number...
+        // Console.Write("guess the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // Using a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(20, 107);
+
+        int guess = 11;
+
+        //using do_while loop
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
